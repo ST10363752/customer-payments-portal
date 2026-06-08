8e3a09c3fe8310ea015aac32132ed1e4b8baf726
Asanda Ngiba ST10363752 <131407198+ST10363752@users.noreply.github.com>
2026-06-08 02:49:53 +0200

    .

--- server/sonar-project.js
+++ server/sonar-project.js
@@ -1,16 +1,14 @@
-const scanner = require('sonarqube-scanner');
+const { Scanner } = require('sonarqube-scanner');
 
-scanner(
-  {
-    serverUrl: 'https://sonarcloud.io',
-    token: process.env.SONAR_TOKEN,
-    options: {
-      'sonar.projectKey': 'ST10363752_customer-payments-portal',
-      'sonar.projectName': 'customer-payments-portal',
-      'sonar.sources': '.',
-      'sonar.exclusions': '**/node_modules/**,**/dist/**,**/build/**',
-      'sonar.javascript.lcov.reportPaths': '',
-    },
+const scanner = new Scanner({
+  serverUrl: 'https://sonarcloud.io',
+  token: process.env.SONAR_TOKEN,
+  options: {
+    'sonar.projectKey': 'ST10363752_customer-payments-portal',
+    'sonar.projectName': 'customer-payments-portal',
+    'sonar.sources': '.',
+    'sonar.exclusions': '**/node_modules/**,**/dist/**,**/build/**',
   },
-  () => process.exit()
-);
+});
+
+scanner.start();
